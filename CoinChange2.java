aaf05df45733ddeceacb0cf78c11d40fbe8ad248
krishnadeepuv <88802563+krishnadeepuv@users.noreply.github.com>
2022-02-20 18:29:46 -0600

    coin change 2 exponentail

--- CoinChange2.java
+++ CoinChange2.java
@@ -1,21 +1,25 @@
-import java.util.Arrays;
 
 public class CoinChange2 {
 
     public int change(int amount, int[] coins) {
-        if (coins == null || coins.length <= 0 || amount <= 0)
-            return 0;
-        int maxWays = 0;
 
-        for (int i = coins.length; i > 0; i++) {
-            maxWays += helper(amount, Arrays.copyOfRange(coins, 0, i), 0, 0, 0);
-        }
-        return maxWays;
+        return helper(amount, coins, 0, 0);
     }
 
-    public int helper(int amount, int[] coins, int index, int sumValue, int crrWays) {
-        if (index < 0)
-            return crrWays;
+    public int helper(int amount, int[] coins, int index, int sum) {
+
+        if (sum > amount || index >= coins.length)
+            return 0;
+        if (sum == amount)
+            return 1;
+
+        // choose
+        int case1 = helper(amount, coins, index, sum + coins[index]);
+        // not choose
+        int case2 = helper(amount, coins, index + 1, sum);
+
+        return case1 + case2;
+
     }
 
     public static void main(String[] args) {
