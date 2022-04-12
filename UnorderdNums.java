b8beb9a6b6e48306a70fc184782bf8f18764a471
krishnadeepuv <88802563+krishnadeepuv@users.noreply.github.com>
2022-04-12 11:17:53 -0500

    Arrays

---
+++ UnorderdNums.java
@@ -0,0 +1,27 @@
+
+public class UnorderdNums {
+
+    public int unOrderCounter(int[] nums) {
+
+        if (nums == null || nums.length == 0)
+            return -1;
+
+        int resCount = 0;
+
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i - 1] > nums[i] || nums[i] > nums[i + 1]) {
+                resCount++;
+                i++;
+            }
+        }
+
+        return resCount;
+    }
+
+    public static void main(String[] args) {
+        UnorderdNums un = new UnorderdNums();
+
+        System.out.println(un.unOrderCounter(new int[] { 1, 2, 4, 3, 7, 6, 7, 8 }));
+        System.out.println(un.unOrderCounter(new int[] { 1, 2, 2, 3, 7, 6, 7, 8, 11, 10 }));
+    }
+}
