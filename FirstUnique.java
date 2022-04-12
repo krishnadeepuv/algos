9a101a20e185f5c4972ac4e5f5be2cd5249a9b2c
krishnadeepuv <88802563+krishnadeepuv@users.noreply.github.com>
2022-04-12 10:51:55 -0500

    Strings

---
+++ FirstUnique.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashMap;
+
+public class FirstUnique {
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character, Integer> hs = new LinkedHashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (hs.containsKey(ch)) {
+                hs.put(ch, -1);
+            } else {
+                hs.put(ch, i);
+            }
+        }
+        for (Character key : hs.keySet()) {
+            if (hs.get(key) >= 0)
+                return hs.get(key);
+        }
+
+        return -1;
+    }
+}
---
+++ MaxChar.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class MaxChar {
+
+    public char maxchar(String s) {
+
+        if (s == null || s.length() == 0)
+            return Character.MIN_VALUE;
+
+        HashMap<Character, Integer> hs = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (hs.containsKey(c)) {
+                hs.put(c, hs.get(c) + 1);
+            } else
+                hs.put(c, 1);
+        }
+
+        int max = Integer.MIN_VALUE;
+        char res = '1';
+        for (Character key : hs.keySet()) {
+            if (hs.get(key) > max) {
+                max = hs.get(key);
+                res = key;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        MaxChar mc = new MaxChar();
+        System.out.println(mc.maxchar("aaaaksjdhfnhhhfjusjdfhlkndvifjjjjjjjjjvnvh"));
+    }
+}
